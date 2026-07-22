0e22d87cc5694ee946765ff3fe48c220c42902b3
Rambabu Bevara <rambabubevara004@gmail.com>
2026-07-22 12:12:29 +0530

    Secure Gemini API key using .env
    Refactor to load API key from environment variables using dotenv.

--- modules/ai.py
+++ modules/ai.py
@@ -1,13 +1,22 @@
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
 
-# Replace with your API key temporarily
-API_KEY = "Api Key "
+# Load environment variables
+load_dotenv()
 
+# Read API key from .env
+API_KEY = os.getenv("GEMINI_API_KEY")
+
+# Configure Gemini
 genai.configure(api_key=API_KEY)
 
+# Create model
 model = genai.GenerativeModel("gemini-2.5-flash")
-
 
 def generate_insights(prompt):
     response = model.generate_content(prompt)
     return response.text
+
+
+
