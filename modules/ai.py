c34433d0603333ed1d2019b8f8dd337f032bc75d
Rambabu Bevara <rambabubevara004@gmail.com>
2026-07-23 19:01:51 +0530

    Update ai.py

--- modules/ai.py
+++ modules/ai.py
@@ -1,18 +1,15 @@
-import os
-from dotenv import load_dotenv
+import streamlit as st
 import google.generativeai as genai
 
-# Load environment variables
-load_dotenv()
-
-# Read API key from .env
-API_KEY = os.getenv("GEMINI_API_KEY")
+# Read API key from Streamlit Secrets
+API_KEY = st.secrets["GEMINI_API_KEY"]
 
 # Configure Gemini
 genai.configure(api_key=API_KEY)
 
 # Create model
 model = genai.GenerativeModel("gemini-2.5-flash")
+
 
 def generate_insights(prompt):
     response = model.generate_content(prompt)
