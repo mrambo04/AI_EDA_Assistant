6652f2d2b392e9f4e4bfabdb30fcc4cb9b3cded0
Rambabu Bevara <rambabubevara004@gmail.com>
2026-07-24 11:43:56 +0530

    Enhance generate_insights with error handling
    Added error handling for API quota and general exceptions in the generate_insights function.

--- modules/ai.py
+++ modules/ai.py
@@ -1,19 +1,32 @@
-import streamlit as st
+import os
+from dotenv import load_dotenv
 import google.generativeai as genai
+from google.api_core.exceptions import ResourceExhausted
 
-# Read API key from Streamlit Secrets
-API_KEY = st.secrets["GEMINI_API_KEY"]
+load_dotenv()
 
-# Configure Gemini
+API_KEY = os.getenv("GEMINI_API_KEY")
+
 genai.configure(api_key=API_KEY)
 
-# Create model
 model = genai.GenerativeModel("gemini-2.5-flash")
 
-
 def generate_insights(prompt):
-    response = model.generate_content(prompt)
-    return response.text
+    try:
+        response = model.generate_content(prompt)
+        return response.text
+
+    except ResourceExhausted:
+        return """
+🚫 **Gemini API quota exceeded**
+
+You have reached the free API request limit.
+
+Please wait for the quota to reset or use a new API key.
+"""
+
+    except Exception as e:
+        return f"❌ Error: {str(e)}"
 
 
 
